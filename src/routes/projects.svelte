9e3d9a21830bc9f635bc13c97acb208846f422b7
Kapsulon <barytheo@gmail.com>
2023-04-26 20:45:51 +0200

    ADD: projects

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -8,3 +8,20 @@
 </head>
 
 <Navbar />
+
+<div class="projects">
+    <div class="project-card">
+        <a href="https://github.com/Kapsulon/klc" target="_blank">
+            <span class="project-name">Kapsulon's Logo Creator</span>
+            <br/>
+            <span class="project-desc">A project that I use to change the colors of my logo from time to time, compiled automatically via github actions.</span>
+        </a>
+    </div>
+    <div class="project-card">
+        <a href="https://github.com/Kapsulon" target="_blank">
+            <span class="project-name">My Github</span>
+            <br/>
+            <span class="project-desc">My Github profile, where I host all my projects.</span>
+        </a>
+    </div>
+</div>
